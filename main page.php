6927b77e3389e092de4538d2ff3e6e1e17e01932
Sheetfan <buzwane.bm@gmail.com>
2023-11-28 19:48:28 +0200

    addeding the styling for the dashboard

--- main page.php
+++ main page.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="Style/DashBoard style.css"> -->
     <!-- <link rel="stylesheet" href="Style/Transaction style.css"> -->
-    <link rel="stylesheet" href="Style/Transaction History style.css">
+    <!-- <link rel="stylesheet" href="Style/Transaction History style.css"> -->
     <!-- <link rel="stylesheet" href="Style/Notification style.css"> -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
@@ -14,27 +14,31 @@
 </head>
 
 <body>
-    <div class="sidebar">
-        <h2>MetroConnect</h2>
-        <ul>
-            <li id="dashboard">
+    <nav class="navbar">
+        <div class="logo-frame">
+            <h2>MetroConnect</h2>
+        </div>
+
+        <ul class="navbar-frame">
+            <li class="dashboard">
                 <i class="fa fa-home" aria-hidden="true"></i>
                 Dashboard
             </li>
-            <li class="active" id="history">
+            <li class="history">
                 <i class="fa fa-exchange" aria-hidden="true"></i>
                 History
             </li>
-            <li id="notifications">
+            <li class="notifications">
                 <i class="fa fa-bell" aria-hidden="true"></i>
                 Notifications
             </li>
-            <li>
-                <i class="fa fa-sign-out" aria-hidden="true"></i>
-                Log out
-            </li>
+
         </ul>
-    </div>
+        <div class = "signout-frame">
+            <i class="fa fa-sign-out" aria-hidden="true"></i>
+            Log out
+        </div>
+    </nav>
     <div class="main-content">
         <header>
             <h2 class="heading-name">Dashboard</h2>
@@ -50,7 +54,7 @@
 
         </div>
     </div>
-    <script src="js/main page.js"> </script>
+    <!-- <script src="js/main page.js"> </script> -->
 </body>
 
 </html>
